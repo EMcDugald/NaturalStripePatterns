c951fb1e7e61f8aa4a7eec83a89aa38eaef6a76d
EMcdugald <edward.mcdugald@gmail.com>
2024-04-18 15:07:00 -0700

    more oral comp stuff

--- HilbertTransform/phase_est.py
+++ HilbertTransform/phase_est.py
@@ -10,15 +10,15 @@ dir = os.getcwd()+"/data/"
 data = sio.loadmat(dir+"mu={:.3f}".format(mu)+".mat")
 
 Ny, Nx, _ = np.shape(data['uu'])
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
 pattern = data['uu'][int(7*Ny/32):int(9*Ny/32),int(Nx/4):int(3*Nx/4),10]
 xx = data['xx'][int(Nx/4):int(3*Nx/4),0]
 yy = data['yy'][int(7*Ny/32):int(9*Ny/32),0]
-im = ax.imshow(pattern,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
-fig.colorbar(im, ax=ax)
-ax.set_title("pattern")
-plt.tight_layout()
-plt.show()
+# im = ax.imshow(pattern,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
+# fig.colorbar(im, ax=ax)
+# ax.set_title("pattern")
+# plt.tight_layout()
+# plt.show()
 
 ny, nx = np.shape(pattern)
 phase = np.zeros(shape=(ny,nx))
@@ -32,36 +32,45 @@ for i in range(ny):
 smooth_unwrapped_phase = np.zeros(shape=(ny,nx))
 for i in range(nx):
     smooth_unwrapped_phase[:,i] = np.unwrap(unwrapped_phase[:,i])
+#
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
+# im = ax.imshow(phase,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
+# ax.set_title("phase")
+# fig.colorbar(im, ax=ax)
+# plt.tight_layout()
+# plt.show()
+#
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
+# im = ax.imshow(unwrapped_phase,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
+# ax.set_title("unwrapped phase")
+# fig.colorbar(im, ax=ax)
+# plt.tight_layout()
+# plt.show()
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
-im = ax.imshow(phase,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
-ax.set_title("phase")
-fig.colorbar(im, ax=ax)
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
+# im = ax.imshow(smooth_unwrapped_phase,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
+# ax.set_title("smooth unwrapped phase")
+# fig.colorbar(im, ax=ax)
+# plt.tight_layout()
+# plt.show()
+#
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
+# im = ax.imshow(np.cos(smooth_unwrapped_phase),cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
+# ax.set_title("recovered pattern")
+# fig.colorbar(im, ax=ax)
+# plt.tight_layout()
+# plt.show()
+
+fig, ax = plt.subplots(nrows=3,ncols=1)
+im1 = ax[0].imshow(smooth_unwrapped_phase,extent=[yy[0],yy[-1],xx[0],xx[-1]])
+im2 = ax[1].imshow(pattern,extent=[yy[0],yy[-1],xx[0],xx[-1]])
+im3 = ax[2].imshow(np.cos(smooth_unwrapped_phase),extent=[yy[0],yy[-1],xx[0],xx[-1]])
+plt.colorbar(im1,ax=ax[0])
+plt.colorbar(im2,ax=ax[1])
+plt.colorbar(im3,ax=ax[2])
+plt.suptitle("Estimated Phase, Actual Pattern, cos(phase)")
 plt.tight_layout()
-plt.show()
-
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
-im = ax.imshow(unwrapped_phase,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
-ax.set_title("unwrapped phase")
-fig.colorbar(im, ax=ax)
-plt.tight_layout()
-plt.show()
-
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
-im = ax.imshow(smooth_unwrapped_phase,cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
-ax.set_title("smooth unwrapped phase")
-fig.colorbar(im, ax=ax)
-plt.tight_layout()
-plt.show()
-
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(3,3))
-im = ax.imshow(np.cos(smooth_unwrapped_phase),cmap='copper',extent=[yy[0],yy[-1],xx[0],xx[-1]])
-ax.set_title("recovered pattern")
-fig.colorbar(im, ax=ax)
-plt.tight_layout()
-plt.show()
-
-
+plt.savefig(os.getcwd()+"/figs/HilbertPhaseMu_{}.png".format(mu))
 
 
 
---
+++ PhaseExtractNN/RCN_Reg_AutoEncoder_KB_v2.py
@@ -0,0 +1,153 @@
+import scipy.io as sio
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+
+dir = os.getcwd()+"/data/"
+data = sio.loadmat(dir+"nn_data_rcn_kb.mat")
+ws = data['ws']
+phases = data['thetas']
+wxs = data['wxs']
+wxxs = data['wxxs']
+wys = data['wys']
+wyys = data['wyys']
+
+nsamps,ny,nx = np.shape(ws)
+perm = np.random.permutation(int(.9*nsamps))
+training_samples = perm[:int(.8*nsamps)]
+val_samples = perm[int(.8*nsamps):]
+test_samples = np.arange(int(.9*nsamps), nsamps)
+
+training_data = {'ws': ws[training_samples,:,:],
+                 'wxs': wxs[training_samples,::4,::4],
+                 'wxxs': wxxs[training_samples,::4,::4],
+                  'wys': wys[training_samples,::4,::4],
+                 'wyys': wyys[training_samples,::4,::4]}
+
+val_data = {'ws': ws[val_samples,:,:],
+                 'wxs': wxs[val_samples,::4,::4],
+                 'wxxs': wxxs[val_samples,::4,::4],
+                  'wys': wys[val_samples,::4,::4],
+                 'wyys': wyys[val_samples,::4,::4]}
+
+test_data = {'ws': ws[test_samples,:,:],
+                 'wxs': wxs[test_samples,::4,::4],
+                 'wxxs': wxxs[test_samples,::4,::4],
+                  'wys': wys[test_samples,::4,::4],
+                 'wyys': wyys[test_samples,::4,::4]}
+
+w_train = training_data['ws']
+w_test = test_data['ws']
+w_val = val_data['ws']
+w_train = w_train.astype('float32')
+w_test = w_test.astype('float32')
+w_val = w_val.astype('float32')
+print(w_train.shape)
+print(w_test.shape)
+print(w_val.shape)
+
+wx_train = training_data['wxs']
+wx_train = wx_train.astype('float32')
+wxx_train = training_data['wxxs']
+wxx_train = wxx_train.astype('float32')
+wy_train = training_data['wys']
+wy_train = wy_train.astype('float32')
+wyy_train = training_data['wyys']
+wyy_train = wyy_train.astype('float32')
+
+import tensorflow as tf
+from tensorflow.keras import layers, losses, Model
+
+class Autoencoder(Model):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Flatten(),
+            layers.Dense(256, activation='linear'),
+            layers.Dense(256, activation='linear'),
+            layers.Dense(256, activation='relu'),
+            layers.Dense(512, activation='relu'),
+            layers.Dense(1024, activation='linear'),
+            layers.Reshape((32,32))
+        ])
+        self.decoder = tf.keras.Sequential([
+            layers.Flatten(),
+            layers.Dense(64, activation='relu'),
+            layers.Dense(784, activation='sigmoid'),
+            layers.Dense(16384, activation='linear'),
+            layers.Reshape((128, 128))# Adjust this based on your input dimensions
+        ])
+
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+def compute_loss(model, x, pre1, pre2, pre3, pre4):
+    with tf.GradientTape(persistent=True) as tape:
+        tape.watch(x)
+        reconstructed = model(x)
+        encoded = model.encoder(x)
+        # First derivative
+        first_derivatives = tape.gradient(encoded, x)
+    # Second derivatives
+    second_derivatives = tape.gradient(first_derivatives, x)
+    del tape  # Clean up the tape
+
+    # Mean squared error loss
+    mse_loss = tf.reduce_mean(tf.square(tf.subtract(reconstructed, x)))
+    # Regularization from derivatives
+    thetaxx = tf.multiply(second_derivatives[:,::4,::4],tf.square(pre1)) + tf.multiply(first_derivatives[:,::4,::4],pre2)
+    thetayy = tf.multiply(second_derivatives[:,::4,::4],tf.square(pre3)) + tf.multiply(first_derivatives[:,::4,::4],pre4)
+    divk = thetaxx + thetayy
+    thetax = tf.multiply(first_derivatives[:,::4,::4],pre1)
+    thetay = tf.multiply(first_derivatives[:,::4,::4],pre3)
+    ksq = tf.square(thetax)+tf.square(thetay)
+    kfth = tf.square(ksq)
+    self_dual_rhs = tf.square(divk)-1+2*ksq -kfth
+    sd_loss = tf.reduce_mean(tf.square(self_dual_rhs))
+    total_loss = mse_loss + 10 * sd_loss
+    return total_loss
+
+# Training the model
+@tf.function
+def train_step(model, input, pre1, pre2, pre3, pre4, optimizer):
+    with tf.GradientTape() as tape:
+        loss = compute_loss(model, input, pre1, pre2, pre3, pre4)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss
+
+
+# Batching the data using TensorFlow's Dataset API
+batch_size = 80
+main_dataset = tf.data.Dataset.from_tensor_slices(w_train).batch(batch_size)
+precomputed_dataset = tf.data.Dataset.zip((
+    tf.data.Dataset.from_tensor_slices(wx_train).batch(batch_size),
+    tf.data.Dataset.from_tensor_slices(wxx_train).batch(batch_size),
+    tf.data.Dataset.from_tensor_slices(wy_train).batch(batch_size),
+    tf.data.Dataset.from_tensor_slices(wyy_train).batch(batch_size),
+))
+
+# Combine main data with precomputed data
+combined_dataset = tf.data.Dataset.zip((main_dataset, precomputed_dataset))
+
+# Define the model, optimizer, and train function if not already defined
+autoencoder = Autoencoder()
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5)
+
+# Function to train the model
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        for batch in dataset:
+            input_data, precomputed_data = batch
+            loss = train_step(autoencoder, input_data, *precomputed_data, optimizer)
+            print(f'Epoch {epoch + 1}, Loss: {loss.numpy()}')
+
+# Start training
+train(combined_dataset, epochs=500)
+
+print("debug")
+encoded_images = autoencoder.encoder(w_test).numpy()
+print("debug")
